5e876f67b60253682dfaad8fb8d106c24c6c7ff9
Tristan Allaman <tristanallaman@gmail.com>
2022-10-19 19:34:01 -0700

    fist commit

---
+++ guessing-game.js
@@ -0,0 +1,22 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let secretNumber = 37
+
+let checkGuess = function(num){
+    if(num > secretNumber){
+        console.log("Too high")
+    }
+
+
+}
+
+
+
+
+
+}
