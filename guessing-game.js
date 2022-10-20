5b897a20d7125c0aad11964b73e6bad7577a7d0d
Tristan Allaman <tristanallaman@gmail.com>
2022-10-19 21:06:39 -0700

    Game Project Solution

--- guessing-game.js
+++ guessing-game.js
@@ -10,13 +10,59 @@ let secretNumber = 37
 let checkGuess = function(num){
     if(num > secretNumber){
         console.log("Too high")
+        return false
+
+
+    } if(num < secretNumber){
+        console.log("Too low")
+        return false
+    } if(num === secretNumber){
+        console.log("Correct")
+        return true
+    }
+
+}
+
+let askGuess = function(){
+    rl.question("Guess what number?",guess => {
+
+      let result = checkGuess(Number(guess))
+
+
+
+
+     if(result === true){
+        console.log("You win!")
+        rl.close()
+
+
+    }
+
+     else {
+        askGuess()
+
+        }
+
+     })
     }
 
 
-}
+
+
+    // rl.question("Guess what number?",guess => {
+    //     rl.close()
+    //     checkGuess(Number(guess))
 
 
 
 
 
-}
+
+
+
+
+
+askGuess()
+
+
+// console.log(checkGuess(37))
